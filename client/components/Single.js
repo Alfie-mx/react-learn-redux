31ff7abe8386cefa2eacbcc089362592788f4387
Alfie-mx <dev@alfredordz.com>
2017-03-25 17:41:07 -0700

    Added Photo and Comments components in Single post. Video 13

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import Photo from './Photo'
+import Comments from './Comments'
 
 
 const Single = React.createClass({
     render(){
+
+        // index of the post
+        const idx = this.props.posts.findIndex( (post) => post.code === this.props.params.postId );
+
+        // get the post
+        const post = this.props.posts[idx];
+
         return (
             <div className="single-photo">
-                I'm the single
+                <Photo idx={idx} post={post} {...this.props} />
+                <Comments/>
             </div>
         )
     }
