b3af8a16bca8980d2da9476f1181b15cf98b650b
Alfie-mx <dev@alfredordz.com>
2017-03-25 17:07:14 -0700

    Displaying Redux State inside our Components. Video 11

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Photo from './Photo'
 
 
 const PhotoGrid = React.createClass({
     render(){
         return (
-            <div className="single-grid">
-                I'm the photogrid
+            <div className="photo-grid">
+                {this.props.posts.map(
+                    (post, idx) => <Photo {...this.props}
+                                          key={idx}
+                                          idx={idx}
+                                          post={post} />
+                )}
             </div>
         )
     }
