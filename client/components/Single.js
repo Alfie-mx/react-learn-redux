7d2cc136b71186fe94e976847596c483a5e26424
Alfie-mx <dev@alfredordz.com>
2017-03-25 17:52:21 -0700

    Displaying and Adding Comments in Single post. Video 14

--- client/components/Single.js
+++ client/components/Single.js
@@ -6,16 +6,21 @@ import Comments from './Comments'
 const Single = React.createClass({
     render(){
 
-        // index of the post
-        const idx = this.props.posts.findIndex( (post) => post.code === this.props.params.postId );
+        const { postId } = this.props.params;
+
+        // index of the post in array
+        const idx = this.props.posts.findIndex( (post) => post.code === postId );
 
         // get the post
         const post = this.props.posts[idx];
 
+        // get the comments
+        const postComments = this.props.comments[postId] || [];
+
         return (
             <div className="single-photo">
                 <Photo idx={idx} post={post} {...this.props} />
-                <Comments/>
+                <Comments postComments={postComments} />
             </div>
         )
     }
